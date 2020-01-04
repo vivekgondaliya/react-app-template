1868b57d44369bb05762de5858a56398c6835522
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-01-04 15:47:04 -0500

    renaming contentContainer to layout

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Header} from 'semantic-ui-react'
+
+
+const Layout = () => {
+    return (
+        <div>
+            <Header as='h1'>Layout Container</Header>
+        </div>
+    )
+}
+
+export default Layout;
