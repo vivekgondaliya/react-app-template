9c54e7c0ff6e369c2a91c2fa1a98f636cf6c4eaa
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-01-04 16:25:15 -0500

    added another section

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 
 import NotFound from './NotFound';
+import AnotherSection from './AnotherSection';
 import Section from './Section';
+import Home from './Home';
 
 const ContentContainer = () => {
     return (
         <Switch>
+            <Route path='/' exact component={Home} />
             <Route path='/section' component={Section} />
+            <Route path='/another-section' component={AnotherSection} />
             <Route component={NotFound} />
         </Switch>
     )
