591c5f24a6b9b7c3bef8f1e9e569f6bf19726122
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-02-01 18:20:17 -0500

    form submission

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -3,23 +3,30 @@ import React, { Component } from 'react'
 class LoginForm extends Component{
     state = {}
 
+    handleSubmit = e => {
+        e.preventDefault();
+
+        //call server
+        console.log("Form Submitted");
+    }
+
     render(){
         return (
             <div>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label for="email">Email</label>
+                        <label htmlFor="email">Email</label>
                         <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
-                        <label for="password">Password</label>
+                        <label htmlFor="password">Password</label>
                         <input type="password" className="form-control" id="password" placeholder="Password" />
                     </div>
                     <div className="form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
