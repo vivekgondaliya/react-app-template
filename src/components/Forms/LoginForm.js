8bb43d5bfc74d16de562b94b6d9d874aeee7cf53
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-02-01 19:05:07 -0500

    basic validation setup

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import Input from './Input';
 
+//GOAL: basic validation
 class LoginForm extends Component{
     state = {
         account: {
             email: "",
             password: ""
-        }
+        },
+        errors: {}
+    }
+
+    validate = () => {
+        const errors = {};
+        const { account } = this.state;
+        if(account.email.trim() === '')
+            errors.email = "Email is required";
+        if(account.password.trim() === '')
+            errors.password = "Password is required";
+
+        return Object.keys(errors).length === 0 ? null : errors;
     }
 
     handleSubmit = e => {
         e.preventDefault();
+        
+        //validate input fields
+        const errors = this.validate();
+        this.setState({ errors });
+
+        //return if errors
+        if(errors) return;
+
         //call server
         console.log("Form Submitted", this.state.account);
     }
