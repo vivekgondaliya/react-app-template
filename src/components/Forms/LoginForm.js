7abd9d64519cf04c0d604a635a4220bec188d233
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2020-02-01 20:14:04 -0500

    reusable FORM component

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from './Input';
+import Form from './Form';
 
-//GOAL: validating a field using joi
-class LoginForm extends Component{
+//GOAL: resusable form component
+/**
+ * Future Forms
+ * 
+ * 1. init state - data
+ * 2. set schema for form
+ * 3. doSubmit() - call server
+ * 4. render()
+ * **/
+class LoginForm extends Form{
     state = {
-        account: {
+        data: {
             email: "",
             password: ""
         },
@@ -17,54 +26,13 @@ class LoginForm extends Component{
         password: Joi.string().required().label('Password')
     }
 
-    validate = () => {
-        const options = { abortEarly : false };
-        const { error } = Joi.validate(this.state.account, this.schema, options);
-        if(!error)return null;
-
-        const errors = {};
-        for (let item of error.details) {
-            errors[item.path[0]] = item.message;
-        }
-        return errors;
-    }
-
-    validateProperty = ({ name, value }) => {
-        const obj = { [name] : value };
-        const schema = { [name] : this.schema[name]};
-        const { error } = Joi.validate(obj, schema);
-    
-        return error ? error.details[0].message : null; 
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-        
-        //validate input fields
-        const errors = this.validate();
-        this.setState({ errors : errors || {} });
-
-        //return if errors
-        if(errors) return;
-
+    doSubmit = () => {
         //call server
-        console.log("Form Submitted", this.state.account);
-    }
-
-    handleChange = ({ currentTarget : input }) => {
-        const errors = {...this.state.errors};
-        const errorMessage = this.validateProperty(input);
-        if(errorMessage) errors[input.name] = errorMessage;
-        else delete errors[input.name];
-
-        const account = {...this.state.account};
-        account[input.name] = input.value;
-
-        this.setState({ account, errors });
+        console.log("Form Submitted", this.state.data);
     }
 
     render(){
-        const { account, errors } = this.state;
+        const { data, errors } = this.state;
         return (
             <div>
                 <h1>Login</h1>
@@ -72,7 +40,7 @@ class LoginForm extends Component{
                     <Input 
                         name="email"
                         label="Email" 
-                        value={account.email}
+                        value={data.email}
                         error={errors.email}
                         onChange={this.handleChange}
                         placeholderText="Enter email"
@@ -80,7 +48,7 @@ class LoginForm extends Component{
                     <Input 
                         name="password"
                         label="Password" 
-                        value={account.password}
+                        value={data.password}
                         error={errors.password}
                         onChange={this.handleChange}
                         placeholderText="Password"
